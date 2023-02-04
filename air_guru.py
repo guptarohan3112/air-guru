db090d5be53fe70af61ab11a5895d98fdf555f2e
ryanling777 <ryannicling@gmail.com>
2023-02-04 21:40:57 +0000

    Integrated Holistic instead of Hand

--- air_guru.py
+++ air_guru.py
@@ -2,15 +2,16 @@ import mediapipe as mp
 import cv2
 from utils import classify_hand
 
-mp_hands = mp.solutions.hands
+# mp_hands = mp.solutions.hands
+mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
+with mp_holistic.Holistic(
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+    model_complexity=2,
+    min_tracking_confidence = 0.5) as holistic:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -22,21 +23,32 @@ with mp_hands.Hands(
     # pass by reference.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    results = holistic.process(image)
       
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+    if results.right_hand_landmarks:
+      #for hand_landmarks in results.right_hand_landmarks:
+      mp_drawing.draw_landmarks(
+          image,
+          results.right_hand_landmarks,
+          mp_holistic.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
         
-        classify_hand(hand_landmarks)
+      classify_hand(results.right_hand_landmarks)
+
+    if results.left_hand_landmarks:
+      #for hand_landmarks in results.right_hand_landmarks:
+      mp_drawing.draw_landmarks(
+          image,
+          results.left_hand_landmarks,
+          mp_holistic.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+        
+      classify_hand(results.left_hand_landmarks)
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import mediapipe as mp
 
-mp_hands = mp.solutions.hands
+# mp_hands = mp.solutions.hands
+mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
@@ -16,28 +17,28 @@ def classify_hand(hand_landmarks):
   translation = lambda fingerIsUp: "UP" if fingerIsUp else "DOWN"
   
   index_finger_is_up = translation(finger_is_up(
-      hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_PIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
+      hand_landmarks.landmark[mp_holistic.HandLandmark.INDEX_FINGER_MCP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.INDEX_FINGER_PIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.INDEX_FINGER_DIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.INDEX_FINGER_TIP].y
   ))
   middle_finger_is_up = translation(finger_is_up(
-      hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_PIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y
+      hand_landmarks.landmark[mp_holistic.HandLandmark.MIDDLE_FINGER_MCP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.MIDDLE_FINGER_PIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.MIDDLE_FINGER_DIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.MIDDLE_FINGER_TIP].y
   ))
   ring_finger_is_up = translation(finger_is_up(
-      hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_PIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y
+      hand_landmarks.landmark[mp_holistic.HandLandmark.RING_FINGER_MCP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.RING_FINGER_PIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.RING_FINGER_DIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.RING_FINGER_TIP].y
   ))
   pinky_is_up = translation(finger_is_up(
-      hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_PIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].y,
-      hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y
+      hand_landmarks.landmark[mp_holistic.HandLandmark.PINKY_MCP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.PINKY_PIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.PINKY_DIP].y,
+      hand_landmarks.landmark[mp_holistic.HandLandmark.PINKY_TIP].y
   ))
   
   print(index_finger_is_up, middle_finger_is_up, ring_finger_is_up, pinky_is_up)
